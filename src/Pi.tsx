059420f6d6b7489e7e8d775c113631df852ee213
john <muffinpost1@gmail.com>
2023-10-12 23:33:00 -0500

    implement more of handleKeypress

--- src/Pi.tsx
+++ src/Pi.tsx
@@ -3,44 +3,72 @@ import { usePiReducer } from "./piModel";
 import { match } from "ts-pattern";
 import { hotkeys } from "./constants";
 import { PiAction } from "./types";
+import * as O from "fp-ts/Option";
+import { pipe } from "fp-ts/lib/function";
 
 export const Pi = () => {
   const [state, dispatch] = usePiReducer();
   const createDispatch = (action: PiAction) => () => () => dispatch(action);
 
   const handleKeypress: React.KeyboardEventHandler<HTMLDivElement> = (e) =>
-    match(e.key)
-      .with(hotkeys.toggleMode, createDispatch({ kind: "toggleMode" }))
-      .with(hotkeys.restartQuiz, createDispatch({ kind: "restartQuiz" }))
-      .with(
-        hotkeys.toggleShowNextDigits,
-        createDispatch({ kind: "toggleShowNextDigits" })
+    pipe(
+      state.keycut,
+      O.match(
+        () =>
+          match(e.key)
+            .with(hotkeys.toggleMode, createDispatch({ kind: "toggleMode" }))
+            .with(hotkeys.restartQuiz, createDispatch({ kind: "restartQuiz" }))
+            .with(
+              hotkeys.toggleShowNextDigits,
+              createDispatch({ kind: "toggleShowNextDigits" })
+            )
+            .with(
+              hotkeys.startGotoKeycut,
+              createDispatch({ kind: "startKeycut", keycut: "goto" })
+            )
+            .with(
+              hotkeys.startSetMarkKeycut,
+              createDispatch({ kind: "startKeycut", keycut: "setMark" })
+            )
+            .with(
+              hotkeys.moveLeftOneDigit,
+              createDispatch({
+                kind: "move",
+                direction: "left",
+                units: "digits",
+              })
+            )
+            .with(
+              hotkeys.moveRightOneDigit,
+              createDispatch({
+                kind: "move",
+                direction: "right",
+                units: "digits",
+              })
+            )
+            .with(
+              hotkeys.moveLeftOneGroup,
+              createDispatch({
+                kind: "move",
+                direction: "left",
+                units: "groups",
+              })
+            )
+            .with(
+              hotkeys.moveRightOneGroup,
+              createDispatch({
+                kind: "move",
+                direction: "right",
+                units: "groups",
+              })
+            )
+            .otherwise(() => () => {}),
+        () =>
+          match(e.key)
+            .with("Escape", createDispatch({ kind: "clearKeycut" }))
+            .otherwise(() => () => {})
       )
-      .with(
-        hotkeys.startGotoKeycut,
-        createDispatch({ kind: "startKeycut", keycut: "goto" })
-      )
-      .with(
-        hotkeys.startSetMarkKeycut,
-        createDispatch({ kind: "startKeycut", keycut: "setMark" })
-      )
-      .with(
-        hotkeys.moveLeftOneDigit,
-        createDispatch({ kind: "move", direction: "left", units: "digits" })
-      )
-      .with(
-        hotkeys.moveRightOneDigit,
-        createDispatch({ kind: "move", direction: "right", units: "digits" })
-      )
-      .with(
-        hotkeys.moveLeftOneGroup,
-        createDispatch({ kind: "move", direction: "left", units: "groups" })
-      )
-      .with(
-        hotkeys.moveRightOneGroup,
-        createDispatch({ kind: "move", direction: "right", units: "groups" })
-      )
-      .otherwise(() => () => {})();
+    )();
 
   return (
     <div style={{ height: "100vh" }} onKeyDown={handleKeypress} tabIndex={-1}>
