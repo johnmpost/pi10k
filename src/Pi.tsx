10eb66b64b88c3bc3e12e5a4e34347e9b751d00d
john <muffinpost1@gmail.com>
2023-10-13 13:25:27 -0500

    move keypress handler to own file

--- src/Pi.tsx
+++ src/Pi.tsx
@@ -1,108 +1,16 @@
 import { Sheet } from "@mui/joy";
 import { usePiReducer } from "./piModel";
-import { P, match } from "ts-pattern";
-import { hotkeys } from "./constants";
-import { PiAction } from "./types";
-import * as O from "fp-ts/Option";
-import { flow, pipe } from "fp-ts/lib/function";
-import {
-  backspace,
-  isDigit,
-  isOneCharacter,
-  stringToDigit,
-  throwIfNone,
-} from "./utils";
+import { handleKeypress } from "./piKeypressHandler";
 
 export const Pi = () => {
   const [state, dispatch] = usePiReducer();
-  const createDispatch = (action: PiAction) => () => () => dispatch(action);
-
-  const handleKeypress: React.KeyboardEventHandler<HTMLDivElement> = (e) =>
-    pipe(
-      state.keycut,
-      O.match(
-        () =>
-          match(e.key)
-            .with(hotkeys.toggleMode, createDispatch({ kind: "toggleMode" }))
-            .with(hotkeys.restartQuiz, createDispatch({ kind: "restartQuiz" }))
-            .with(
-              hotkeys.toggleShowNextDigits,
-              createDispatch({ kind: "toggleShowNextDigits" })
-            )
-            .with(
-              hotkeys.startGotoKeycut,
-              createDispatch({ kind: "startKeycut", keycut: "goto" })
-            )
-            .with(
-              hotkeys.startSetMarkKeycut,
-              createDispatch({ kind: "startKeycut", keycut: "setMark" })
-            )
-            .with(
-              hotkeys.moveLeftOneDigit,
-              createDispatch({
-                kind: "move",
-                direction: "left",
-                units: "digits",
-              })
-            )
-            .with(
-              hotkeys.moveRightOneDigit,
-              createDispatch({
-                kind: "move",
-                direction: "right",
-                units: "digits",
-              })
-            )
-            .with(
-              hotkeys.moveLeftOneGroup,
-              createDispatch({
-                kind: "move",
-                direction: "left",
-                units: "groups",
-              })
-            )
-            .with(
-              hotkeys.moveRightOneGroup,
-              createDispatch({
-                kind: "move",
-                direction: "right",
-                units: "groups",
-              })
-            )
-            .with(
-              P.when(isDigit),
-              (digitStr) => () =>
-                dispatch({
-                  kind: "enterDigit",
-                  digit: flow(stringToDigit, throwIfNone)(digitStr),
-                })
-            )
-            .otherwise(() => () => {}),
-        (keycut) =>
-          match(e.key)
-            .with("Escape", createDispatch({ kind: "clearKeycut" }))
-            .with("Enter", createDispatch({ kind: "executeKeycut" }))
-            .with(
-              "Backspace",
-              createDispatch({
-                kind: "setKeycutParameters",
-                newParameters: backspace(keycut.parameters),
-              })
-            )
-            .with(
-              P.when(isOneCharacter),
-              (digitStr) => () =>
-                dispatch({
-                  kind: "setKeycutParameters",
-                  newParameters: keycut.parameters + digitStr,
-                })
-            )
-            .otherwise(() => () => {})
-      )
-    )();
 
   return (
-    <div style={{ height: "100vh" }} onKeyDown={handleKeypress} tabIndex={-1}>
+    <div
+      style={{ height: "100vh" }}
+      onKeyDown={handleKeypress(state, dispatch)}
+      tabIndex={-1}
+    >
       <Sheet sx={{ height: "100%" }}>
         <pre style={{ margin: 0 }}>{`state: ${JSON.stringify(
           state,
---
+++ src/piKeypressHandler.ts
@@ -0,0 +1,100 @@
+import { P, match } from "ts-pattern";
+import { hotkeys } from "./constants";
+import { PiAction, PiState } from "./types";
+import * as O from "fp-ts/Option";
+import { flow, pipe } from "fp-ts/lib/function";
+import {
+  backspace,
+  isDigit,
+  isOneCharacter,
+  stringToDigit,
+  throwIfNone,
+} from "./utils";
+
+export const handleKeypress =
+  (state: PiState, dispatch: React.Dispatch<PiAction>) =>
+  (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const createDispatch = (action: PiAction) => () => () => dispatch(action);
+    return pipe(
+      state.keycut,
+      O.match(
+        () =>
+          match(e.key)
+            .with(hotkeys.toggleMode, createDispatch({ kind: "toggleMode" }))
+            .with(hotkeys.restartQuiz, createDispatch({ kind: "restartQuiz" }))
+            .with(
+              hotkeys.toggleShowNextDigits,
+              createDispatch({ kind: "toggleShowNextDigits" })
+            )
+            .with(
+              hotkeys.startGotoKeycut,
+              createDispatch({ kind: "startKeycut", keycut: "goto" })
+            )
+            .with(
+              hotkeys.startSetMarkKeycut,
+              createDispatch({ kind: "startKeycut", keycut: "setMark" })
+            )
+            .with(
+              hotkeys.moveLeftOneDigit,
+              createDispatch({
+                kind: "move",
+                direction: "left",
+                units: "digits",
+              })
+            )
+            .with(
+              hotkeys.moveRightOneDigit,
+              createDispatch({
+                kind: "move",
+                direction: "right",
+                units: "digits",
+              })
+            )
+            .with(
+              hotkeys.moveLeftOneGroup,
+              createDispatch({
+                kind: "move",
+                direction: "left",
+                units: "groups",
+              })
+            )
+            .with(
+              hotkeys.moveRightOneGroup,
+              createDispatch({
+                kind: "move",
+                direction: "right",
+                units: "groups",
+              })
+            )
+            .with(
+              P.when(isDigit),
+              (digitStr) => () =>
+                dispatch({
+                  kind: "enterDigit",
+                  digit: flow(stringToDigit, throwIfNone)(digitStr),
+                })
+            )
+            .otherwise(() => () => {}),
+        (keycut) =>
+          match(e.key)
+            .with("Escape", createDispatch({ kind: "clearKeycut" }))
+            .with("Enter", createDispatch({ kind: "executeKeycut" }))
+            .with(
+              "Backspace",
+              createDispatch({
+                kind: "setKeycutParameters",
+                newParameters: backspace(keycut.parameters),
+              })
+            )
+            .with(
+              P.when(isOneCharacter),
+              (digitStr) => () =>
+                dispatch({
+                  kind: "setKeycutParameters",
+                  newParameters: keycut.parameters + digitStr,
+                })
+            )
+            .otherwise(() => () => {})
+      )
+    )();
+  };
