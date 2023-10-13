4b2a3bffe1a1ec69d47298326aeb83fd201de4e4
john <muffinpost1@gmail.com>
2023-10-12 22:38:51 -0500

    add font, add joy ui css stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
 import { Pi } from "./Pi";
 
 export const App = () => {
-  return <Pi></Pi>;
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+      <Pi></Pi>
+    </CssVarsProvider>
+  );
 };
--- src/Pi.tsx
+++ src/Pi.tsx
@@ -1,11 +1,16 @@
+import { Sheet } from "@mui/joy";
 import { usePiReducer } from "./piModel";
 
 export const Pi = () => {
   const [state, dispatch] = usePiReducer();
 
   return (
-    <>
-      <div>{`mode: ${state.mode.kind}`}</div>
-    </>
+    <Sheet sx={{ height: "100vh", marginTop: "0px" }}>
+      <pre style={{ margin: 0 }}>{`state: ${JSON.stringify(
+        state,
+        null,
+        2
+      )}`}</pre>
+    </Sheet>
   );
 };
