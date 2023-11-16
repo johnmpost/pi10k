71878195b31fa4aa63df48e4b5dbe8c917a87c11
john <muffinpost1@gmail.com>
2023-11-15 23:28:18 -0600

    add Config component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { Pi } from "./app/PiDisplay";
 import { GlobalStateProvider } from "./app/globalState";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ErrorPage } from "./app/ErrorPage";
+import { Config } from "./app/Config";
 
 const router = createBrowserRouter([
   { path: "/", element: <Pi></Pi>, errorElement: <ErrorPage /> },
-  { path: "config", element: <div>config page</div> },
+  { path: "config", element: <Config /> },
 ]);
 
 export const App = () => {
